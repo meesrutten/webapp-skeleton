479f0d8db50e759757e961db6890e12dedfcc926
meesrutten <mees.rutten@cobrasystems.nl>
2018-02-07 11:05:33 +0100

    Add index.html, js structure, style structure, favicons and manifest

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+console.log('test');
+
+// // Import a module using CommonJS syntax
+// const dep = require('./path/to/dep');
+
+// // Import a module using ES6 import syntax
+// import dep from './path/to/dep';
+
+// // Import a CSS file
+// import './test.css';
+
+// // Import a CSS file with CSS modules
+// import classNames from './test.css';
+
+// // Import the URL to an image file
+// import imageURL from './test.png';
